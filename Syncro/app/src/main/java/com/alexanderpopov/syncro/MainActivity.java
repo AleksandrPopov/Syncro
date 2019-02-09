8d1dc929586c9377409512d6ad9c4697806115a8
AlexanderPopov <stremis89@gmail.com>
2019-02-09 13:17:09 +0200

    Start

--- Syncro/app/src/main/java/com/alexanderpopov/syncro/MainActivity.java
+++ Syncro/app/src/main/java/com/alexanderpopov/syncro/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String s = "hello";
+        String s2 = "hello";
     }
 }
